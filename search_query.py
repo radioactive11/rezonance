893047584b340fd3b1057db9e8e2c55cf4454468
radiactive11 <roy.arijit2001@gmail.com>
2020-09-08 12:19:45 +0530

    limited search results to 10

--- search_query.py
+++ search_query.py
@@ -6,7 +6,10 @@ def query(param):
     mask = ids["search"].str.contains(param, regex=False, case=False)
     df_results = ids[mask]
     search_results = []
+    ctr = 0
     for i in range(len(df_results)):
+        if ctr == 10:
+            break
         song_id = df_results.iloc[i, 0]
         song_name = df_results.iloc[i, 1]
         song_artist = df_results.iloc[i, 2]
@@ -15,5 +18,7 @@ def query(param):
             "artist": song_artist,
             "id": str(song_id)
         }
-        search_results.append(temp_dict)
+        if song_id <= 50000:
+            search_results.append(temp_dict)
+            ctr += 1
     return search_results
