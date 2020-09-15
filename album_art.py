498b08faa9358e75a7316607c7c287d960810fbd
radiactive11 <roy.arijit2001@gmail.com>
2020-09-15 22:25:10 +0530

    added album art scrapper

---
+++ album_art.py
@@ -0,0 +1,28 @@
+import concurrent.futures
+import os
+import re
+import sys
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+
+def album_art(song_name):
+    song_name = song_name.replace(" ", "+")
+    url = "https://www.google.com/search?q=" + song_name + "&tbm=isch"
+    driver = webdriver.Chrome(executable_path=DRIVER_BIN, options=option)
+    driver.get(url)
+    res = driver.execute_script("return document.documentElement.outerHTML")
+    soup = BeautifulSoup(res, 'lxml')
+    main_container = soup.find("img", {"class": "rg_i Q4LuWd"})
+
+    with open("art.txt", 'r+') as f:
+        f.truncate(0)
+        f.write(main_container['src'])
+    f.close()
+
+album_art("neon lights demi lovato")
