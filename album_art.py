1c6ce8fadc64a5f96cdce92e007bc310fd72fa1b
radiactive11 <roy.arijit2001@gmail.com>
2020-09-21 22:46:49 +0530

    updated album_art
    Using Bing instead of Google

--- album_art.py
+++ album_art.py
@@ -12,17 +12,28 @@ option = webdriver.ChromeOptions()
 option.add_argument('headless')
 
 def album_art(song_name):
-    song_name = song_name.replace(" ", "+")
-    url = "https://www.google.com/search?q=" + song_name + "&tbm=isch"
-    driver = webdriver.Chrome(executable_path=DRIVER_BIN, options=option)
-    driver.get(url)
-    res = driver.execute_script("return document.documentElement.outerHTML")
-    soup = BeautifulSoup(res, 'lxml')
-    main_container = soup.find("img", {"class": "rg_i Q4LuWd"})
+    success = 0
+    fail = 0
+    try:
+        song_name = song_name.replace(" ", "+")
+        url = "https://www.bing.com/images/search?q=" + song_name + "&qs=MM&form=QBILPG&sp=1&pq=state+of+grace+tau&sc=8-18&cvid=6B091F9B39314A91B4960D6158ECB55D&first=1&scenario=ImageBasicHover"
+        driver = webdriver.Chrome(executable_path=DRIVER_BIN, options=option)
+        driver.get(url)
+        res = driver.execute_script("return document.documentElement.outerHTML")
+        soup = BeautifulSoup(res, 'lxml')
+        main_container = soup.find("img", {"class": "mimg"})
+        # print(main_container["src"])
+        image_id = main_container['src']
+        success += 1
+        print("success for: ", song_name)
+        driver.quit()
+        print(image_id)
+        return image_id
+    
+    except:
+        fail += 1
+        
+        return "no_img"
+    
 
-    with open("art.txt", 'r+') as f:
-        f.truncate(0)
-        f.write(main_container['src'])
-    f.close()
-
-album_art("neon lights demi loavto")
+album_art("faded alan walker")
