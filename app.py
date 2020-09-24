58cb54782f70ca8e6dc24387dfa8d0a5416c04ff
radiactive11 <roy.arijit2001@gmail.com>
2020-09-24 11:59:09 +0530

    Added CORS supoort

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+from flask_cors import CORS, cross_origin
 import search_query
 import recoms
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
