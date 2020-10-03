5e2c3e1137e8a21cb58e27285f0ae1eda522729e
Kartik Goel <goel.kartik39@gmail.com>
2020-10-04 01:42:48 +0530

    Added search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import axios from "axios"
 
 const App = () => {
+  const [search,setSearch] = useState("");
+  const [result,setResult] = useState({});
+
+  const output = (e) => {
+    e.preventDefault();
+    
+    axios.post("https://rezonance-radioactive11.herokuapp.com/search",{
+      search_param : search
+    })
+    .then((res) => {
+      console.log(res.data);
+      setResult(res.data);
+    })
+  }
+
   return (
-    <div className ="image bg">
-      <h1 className = "heading"> ReZonance </h1>
+    <div>
+      
+      <input 
+        type ="text" 
+        name = "search"
+        value = {search}
+        onChange = {(e) => setSearch(e.target.value)}
+      />
+      <button type = "submit" onClick = {(e) => output(e)} > submit </button>
+      <p>{JSON.stringify(result)}</p>
     </div>
+
   )
   
 }
