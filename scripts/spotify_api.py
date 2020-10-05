9d66393fc54a4582095c956762171f94557e2501
radiactive11 <roy.arijit2001@gmail.com>
2020-10-05 10:49:47 +0530

    error handling for spotify api

--- scripts/spotify_api.py
+++ scripts/spotify_api.py
@@ -4,27 +4,35 @@ import json
 CLIENT_ID = "50179b9e0a064ad09ffb159682151c22"
 CLIENT_SECRET = "acd7f54eea1e4e70a6f35f7afbaee6cb"
 
-AUTH_URL = "https://accounts.spotify.com/api/token"
 
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-})
+try:
+    AUTH_URL = "https://accounts.spotify.com/api/token"
 
-auth_response_data = auth_response.json()
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
 
+    auth_response_data = auth_response.json()
+except Exception as e:
+    print("error in auth")
+    print(e)
 
 # save the access token
-access_token = auth_response_data['access_token']
 
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
+try:
+    access_token = auth_response_data['access_token']
+
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
 
 
-
-BASE_URL = 'https://api.spotify.com/v1/'
+    BASE_URL = 'https://api.spotify.com/v1/'
+except Exception as e:
+    print("error getting access token")
+    print(e)
 
 
 def get_urls(track_id):
@@ -32,7 +40,7 @@ def get_urls(track_id):
         
         # actual GET request with proper header
         res = requests.get("https://api.spotify.com/v1/tracks/?ids={id}".format(id = track_id), headers=headers).json()
-
+        print(res)
         # r_image = r["album"]["images"]
 
         try:
